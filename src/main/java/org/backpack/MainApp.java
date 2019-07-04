ac7ea9d9736cda0143cc4021cfea2887c7024dff
Henrik Jepsen <hekjje@gmail.com>
2019-07-04 09:28:20 +0200

    Added app icon

--- src/main/java/org/backpack/MainApp.java
+++ src/main/java/org/backpack/MainApp.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class MainApp extends Application {
@@ -15,6 +16,7 @@ public class MainApp extends Application {
         Scene scene = new Scene(root);
         scene.getStylesheets().add(getClass().getResource("styles.css").toExternalForm());
 
+        stage.getIcons().add(new Image(getClass().getResourceAsStream("gfx/backpack.png")));
         stage.setTitle("Backpack");
         stage.setScene(scene);
         stage.show();
